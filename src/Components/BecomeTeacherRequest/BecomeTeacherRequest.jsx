80c6df52f0972c9beb58da339e3e84a9c7e7b2e8
asifbuet047 <asifbuet047@gmail.com>
2023-11-29 03:00:17 +0600

    Add a form in teach on page using react-hook check before submit also use react mutation for all post

--- src/Components/BecomeTeacherRequest/BecomeTeacherRequest.jsx
+++ src/Components/BecomeTeacherRequest/BecomeTeacherRequest.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
 import { useForm } from 'react-hook-form';
 import useLoggedinUser from '../../Hooks/useLoggedinUser';
-import { Avatar, Box, Button, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { Avatar, Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { useMutation } from '@tanstack/react-query';
+import useAxiosSecure from '../../Hooks/useAxiosSecure';
+import { Button } from 'flowbite-react';
 
 function BecomeTeacherRequest() {
     const user = useLoggedinUser();
+    const instance = useAxiosSecure();
     const { handleSubmit, register, getValues, formState: { errors } } = useForm();
 
+    const { data, mutate, isPending, isSuccess, isIdle } = useMutation({
+        mutationFn: (data) => {
+            return instance.post('/teachrequest', data);
+        },
+        onSuccess: (data) => {
+            console.log(data);
+        }
+    });
+
     const handleSubmitForReview = () => {
-
-    }
-
-    const handleExperienceChange = (event) => {
-        console.log(event.target.value);
-    }
-
-    const handleCategoryChange = (event) => {
-        console.log(event.target.value);
-
+        const request = {
+            name: user.displayName,
+            email: user.email,
+            status: 'pending',
+            ...getValues()
+        };
+        mutate(request);
     }
 
     return (
@@ -31,7 +41,7 @@ function BecomeTeacherRequest() {
                             <label className="label">
                                 <span className="label-text">Teacher name</span>
                             </label>
-                            <input type="text" value={user.displayName} className="input input-bordered" {...register('name')} disabled />
+                            <input type="text" value={user.displayName} className="input input-bordered" disabled />
                         </div>
 
                         <div className="form-control">
@@ -48,7 +58,7 @@ function BecomeTeacherRequest() {
                             <label className="label">
                                 <span className="label-text">Request title</span>
                             </label>
-                            <input type="email" placeholder="Your request message to admin" className="input input-bordered" {...register('title', { required: true })} />
+                            <input type="text" placeholder="Your request message to admin" className="input input-bordered" {...register('title', { required: true })} />
                             {
                                 errors.title?.type === 'required' && <span className='text-red-600'>Required</span>
                             }
@@ -61,12 +71,16 @@ function BecomeTeacherRequest() {
                                     <Select
                                         labelId="Experience-label"
                                         label="Experience"
-                                        onChange={handleExperienceChange}
+
+                                        {...register('experience', { required: true })}
                                     >
-                                        <MenuItem value={'Teacher'}>Beginner</MenuItem>
+                                        <MenuItem value={'Beginner'}>Beginner</MenuItem>
                                         <MenuItem value={'Experienced'}>Experienced</MenuItem>
                                         <MenuItem value={'SomeIdea'}>Some Idea</MenuItem>
                                     </Select>
+                                    {
+                                        errors.experience?.type === 'required' && <span className='text-red-600'>Required</span>
+                                    }
                                 </FormControl>
                             </Box>
                         </div>
@@ -78,7 +92,8 @@ function BecomeTeacherRequest() {
                                     <Select
                                         labelId="Category-label"
                                         label="Category"
-                                        onChange={handleCategoryChange}
+
+                                        {...register('category', { required: true })}
                                     >
                                         <MenuItem value={'Web Development'}>Web Development</MenuItem>
                                         <MenuItem value={'Ethical Hacking'}>Ethical Hacking</MenuItem>
@@ -86,12 +101,33 @@ function BecomeTeacherRequest() {
                                         <MenuItem value={'Game Development'}>Game Development</MenuItem>
                                         <MenuItem value={'Software Development'}>Software Development</MenuItem>
                                     </Select>
+                                    {
+                                        errors.category?.type === 'required' && <span className='text-red-600'>Required</span>
+                                    }
                                 </FormControl>
                             </Box>
                         </div>
 
                         <div className="form-control mt-6">
-                            <Button variant="contained" type="submit" >Submit for review</Button>
+                            {
+                                isPending &&
+                                <div className='flex flex-row justify-center items-center'>
+                                    <Button className='w-full' isProcessing >Submitting</Button>
+                                </div>
+                            }
+                            {
+                                isSuccess &&
+                                <div className='flex flex-row justify-center items-center'>
+                                    <Button className='w-full' disabled>Submitted Wait for Admin</Button>
+                                </div>
+                            }
+                            {
+                                isIdle &&
+                                <div className='flex flex-row justify-center items-center'>
+                                    <Button className='w-full' type='submit'>Submit for review</Button>
+                                </div>
+                            }
+
                         </div>
                     </form>
                 }
